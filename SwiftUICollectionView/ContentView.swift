7e4bc479aa9200ced0e2466bf8ed632598500e7e
ambercoded <mailforadrian@me.com>
2021-02-14 10:20:18 +0100

    Initial Commit

---
+++ SwiftUICollectionView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICollectionView
+//
+//  Created by Adrian B. Haeske on 14.02.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
